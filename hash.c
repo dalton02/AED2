1127a092a5f0d8e68620bd1827f2286233a079b4
dalton02 <104688487+dalton02@users.noreply.github.com>
2022-12-08 09:12:32 -0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 struct hash{
 	int* valor;
 	int m; // Zona normal
@@ -11,56 +10,37 @@ struct hash{
 typedef struct hash Hash;
 
 Hash* criar(int m,int n){
-
 	Hash* tabela = (Hash*) malloc(sizeof(Hash));
 	tabela->valor = (int*) malloc((n+m)*sizeof(int));
-	
 	for(int i=0;i<(m+n);i++)
 		tabela->valor[i] = NULL;
-	
 	tabela->m = m;
 	tabela->n = n;
 	return tabela;
 }
 
-//Função H(x) = x % mod 4
 void inserir(Hash* tabela,int x,int y){
 	
 	int posicao = gerarHash(x,y);
-	
 	if( tabela->valor[posicao] == NULL )
 		tabela->valor[posicao] = x;
-
 	else{
-
 		int n = tabela->n;
 		int m = tabela->m;
-
 		if(tabela->valor[m] == NULL)
 			tabela->valor[m] = x;
-
 		else{
-			
 			while( tabela->valor[m] != NULL)
 				m++;
-		 	
-		 
 			if(m<(n+m))
-				tabela->valor[m] = x;		
-		
+				tabela->valor[m] = x;			
 		}
 	}
-	
-	
 }
 
 void imprimir(Hash* tabela){
-	
 	int n = tabela->n;
 	int m = tabela->m;
-	
-	//Imprimindo zona normal
-	
 	printf("\n --------------TABELA HASH------------------- \n");
 	int i=0;
 	while(i<(m)){	
@@ -68,8 +48,6 @@ void imprimir(Hash* tabela){
 		i++;
 	}
 	printf("\n --------------ZONA DE COLISAO------------------- \n");
-	
-	//Imprimindo zona de colisão
 	i=m;
 	while(i<(m+n)){
 		printf(" %d ",tabela->valor[i]);
@@ -78,7 +56,6 @@ void imprimir(Hash* tabela){
 }
 
 void deletar(Hash* tabela, int x,int y){
-
 	int posicao = buscar(tabela,x,y);
 	if(posicao != NULL)
 	tabela->valor[posicao] = NULL;
@@ -86,25 +63,17 @@ void deletar(Hash* tabela, int x,int y){
 }
 
 int buscar(Hash* tabela, int x,int y){
-	
 	int posicao = gerarHash(x,y);
-	
-	
 	if(tabela->valor[posicao] == x)
-		return posicao;
-			
+		return posicao;		
 	else{
-	
 		int m = tabela->m;
 		int n = tabela->n;
 		while(tabela->valor[m] != x && m<(n+m))
 			m++;
-			
 		if(tabela->valor[m] == x)
-			return m;
-		
+			return m;		
 	}
-	
 	return NULL;
 }
 
@@ -115,11 +84,12 @@ int gerarHash(int valor,int modulo){
 
 int main(){
 	Hash* tabela = criar(3,5);	//Inserimos o tamanho da zona normal e o tamanho da zona de colisão
-	inserir(tabela,48,4); //(Inserir tabela,Inserir valor,Inserir modulo da função)
-	inserir(tabela,49,4);
-	inserir(tabela,4,4);
-	inserir(tabela,8,4);
-	deletar(tabela,49,4);
+	int modulo=4;
+	inserir(tabela,48,modulo); //(Inserir tabela,Inserir valor,Inserir modulo da função)
+	inserir(tabela,49,modulo);
+	inserir(tabela,4,modulo);
+	inserir(tabela,50,modulo);
+	inserir(tabela,8,modulo);
 	imprimir(tabela);
 	printf("\nvalor 8 esta na posicao: %d ",buscar(tabela,8,4));
 	
